fc5205d9f0ba049b1524c68ebe7c4967cd11f169
subihyun <134204008+subihyun@users.noreply.github.com>
2026-04-20 22:45:01 +0900

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,8 @@ import react from '@vitejs/plugin-react'
 import { VitePWA } from 'vite-plugin-pwa'
 
 export default defineConfig({
-  plugins: [
+  base: '/haeseola-homepage/',
+  plugins: 
     react(),
     VitePWA({
       registerType: 'autoUpdate',
